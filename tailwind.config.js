0128289133fe266af5491479905e1121aa846b8e
Metin Ahmet Erkeleş <erkelesmetin@gmail.com>
2024-05-04 18:43:12 +0300

    custom font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+      fontSize: {
+        huge: ["9rem", { lineHeight: "1" }],
+      },
+    },
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
   },
   plugins: [],
 };

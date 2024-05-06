99206094283fe5c51eaa447a17bbec1496b16012
Metin Ahmet Erkeleş <erkelesmetin@gmail.com>
2024-05-06 13:44:41 +0300

    modelling the cart state

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./features/user/userSlice";
+import cartReducer from "./features/card/cartSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
